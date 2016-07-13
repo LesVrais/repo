3f0aa2970d1874c0f1d9a9f3a694e3a47f17a0ff
Alex <INTI-0277@INTI0277>
2016-07-13 11:26:43 +0200

    commit dao gerant

---
+++ proxiBanque/src/main/java/fr/adaming/dao/GerantDaoImpl.java
@@ -0,0 +1,68 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import fr.adaming.model.Conseiller;
+
+@Repository
+@Transactional
+public class GerantDaoImpl implements IGerantDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	/**
+	 * @return the sessionFactory
+	 */
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	/**
+	 * @param sessionFactory the sessionFactory to set
+	 */
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public List<Conseiller> getAllConseiller() {
+		Session session = sessionFactory.openSession();
+		String reqHQL = "FROM GerantEntity g order by g.id asc";
+		Query query = session.createQuery(reqHQL);
+		List<Conseiller> liste = query.list();
+		return liste;
+	}
+
+	@Override
+	public Conseiller getConseillerById(int id) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addConseiller(Conseiller c) {
+		Session session = sessionFactory.openSession();		session.save(c);
+		session.close();
+	}
+
+	@Override
+	public void deleteConseiller(int id) {
+		
+	}
+
+	@Override
+	public void modifyConseiller(int id, String nom, String prenom) {
+		
+	}
+
+}
